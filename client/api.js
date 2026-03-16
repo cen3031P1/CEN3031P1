7988b74fea86be9006e1b66f63dd056a9cb4100e
csilva1-eng <christophersilva656@gmail.com>
2026-03-16 19:10:57 -0400

    sorry check prev 2 commit messages

--- client/api.js
+++ client/api.js
@@ -1,3 +1,11 @@
+/*
+    requests are weird with react-native. cant just use http://localhost<port>. Instead need to use own laptops IP. 
+    this makes it that whenever you do await api.get('/api/...) it does the whole http://ip thing. also you dont even need to type axios
+    whenever you would put axios just use api.
+
+    To get the API_URL check example.env. I can not give you this one.
+*/
+
 import axios from 'axios'
 
 const api = axios.create({
